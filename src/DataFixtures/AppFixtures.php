7bdfb0d20f850e31983dacf7939b3671ad74d83f
Bogdan Radoi <radoi.office@gmail.com>
2023-04-17 12:23:37 +0200

    Foundry Fixtures, create random 100 Answers + relation

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Question;
+use App\Factory\AnswerFactory;
 use App\Factory\QuestionFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -14,5 +15,22 @@ class AppFixtures extends Fixture
         # unpublished change les valeurs par défaut, c'est une méthode créée
         QuestionFactory::createMany(20);
         QuestionFactory::new()->unpublished()->many(5)->create();
+
+        # Tous les réponses avec la même question
+        # AnswerFactory::createMany(100, [
+        #     # Récupère une Question aléatoire depuis la bdd, Attention: Il faut quelles soient créées à ce moment
+        #     'question' => QuestionFactory::random()
+        # ]);
+
+        # Tous les réponses avec question différente
+        # AnswerFactory::createMany(100, function () {
+        #     return [
+        #         # Récupère une Question aléatoire depuis la bdd, Attention: Il faut quelles soient créées à ce moment
+        #         'question' => QuestionFactory::random()
+        #     ];
+        # });
+
+        # Tous les réponses avec question différente mais fait dans Answer factory
+        AnswerFactory::createMany(100);
     }
 }
