ef645b24c108d74487dbe0847e27619f868c642d
Bogdan Radoi <radoi.office@gmail.com>
2023-05-01 17:43:40 +0200

    N+1 problem ManyToMany fix

--- src/Repository/QuestionRepository.php
+++ src/Repository/QuestionRepository.php
@@ -25,13 +25,25 @@ class QuestionRepository extends ServiceEntityRepository
      */
     public function findAllAskedOrderedByNewest()
     {
+        # Simple ManyToMany
+        # return $this->addIsAskedQueryBuilder()
+        #     ->orderBy('q.askedAt', 'DESC')
+        #     ->leftJoin('q.tags', 'tag') # ManyToMany, tag = alias for the Tag Table
+        #     ->addSelect('tag')              # Fix N + 1 problem on homepage, Select all data from Tag table
+        #     ->getQuery()
+        #     ->getResult()
+        # ;
+
+        # Complexe ManyToMany avec colonne en plus dans la table question_tag
         return $this->addIsAskedQueryBuilder()
             ->orderBy('q.askedAt', 'DESC')
-            ->leftJoin('q.tags', 'tag') # ManyToMany, tag = alias for the Tag Table
-            ->addSelect('tag')              # Fix N + 1 problem on homepage, Select all data from Tag table
+            ->leftJoin('q.questionTags', 'question_tag') # alias for the QuestionTag Table
+            ->innerJoin('question_tag.tag', 'tag')       # ManyToMany, tag = alias for the Tag Table
+            # il faut rajouteur aussi la table d'association questionTag si on veut les données de la tables Tag
+            ->addSelect(['question_tag', 'tag'])                   # Fix N + 1 problem on homepage, Select all data from Tag table
             ->getQuery()
             ->getResult()
-        ;
+            ;
     }
 
     private function addIsAskedQueryBuilder(QueryBuilder $qb = null): QueryBuilder
